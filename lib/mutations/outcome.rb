4ef8f59288da338fadf2917000df809b0032a7dc
Jonathan Novak <jnovak@gmail.com>
2013-03-01 22:37:04 -0800

    Use attr reader a bit

--- lib/mutations/outcome.rb
+++ lib/mutations/outcome.rb
@@ -1,23 +1,13 @@
 module Mutations
   class Outcome
+    attr_reader :result, :errors, :inputs
+    
     def initialize(is_success, result, errors, inputs)
       @success, @result, @errors, @inputs = is_success, result, errors, inputs
     end
 
     def success?
       @success
-    end
-
-    def result
-      @result
-    end
-
-    def errors
-      @errors
-    end
-    
-    def inputs
-      @inputs
     end
   end
 end
