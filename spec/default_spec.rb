a8cb837f362baf4353643f87420a4fe68afc0abf
Jonathan Novak <jnovak@gmail.com>
2013-03-01 20:21:21 -0800

    Add specs for defaults

---
+++ spec/default_spec.rb
@@ -0,0 +1,33 @@
+require_relative 'spec_helper'
+require 'simple_command'
+
+describe 'Mutations - defaults' do
+
+  class DefaultCommand < Mutations::Command
+    required do
+      string :name, default: "Bob Jones"
+    end
+    
+    def execute
+      inputs
+    end
+  end
+  
+  it "should have a default if no value is passed" do
+    outcome = DefaultCommand.run
+    assert_equal true, outcome.success?
+    assert_equal ({"name" => "Bob Jones"}), outcome.result
+  end
+  
+  it "should have the passed value if a value is passed" do
+    outcome = DefaultCommand.run(name: "Fred")
+    assert_equal true, outcome.success?
+    assert_equal ({"name" => "Fred"}), outcome.result
+  end
+  
+  it "should be an error if nil is passed on a required field with a default" do
+    outcome = DefaultCommand.run(name: nil)
+    assert_equal false, outcome.success?
+  end
+  
+end
