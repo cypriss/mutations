182a4e67db46864eff1dfefc87b40c04bdf009e9
Jonathan Novak <jnovak@gmail.com>
2013-05-09 17:03:34 -0700

    Fix tests on 1.8.7 :(

--- spec/date_filter_spec.rb
+++ spec/date_filter_spec.rb
@@ -23,8 +23,12 @@ describe "Mutations::DateFilter" do
     time = Time.now
     f = Mutations::DateFilter.new
     filtered, errors = f.filter(time)
-    assert_equal time.to_date, filtered
-    assert_equal nil, errors
+    if time.respond_to?(:to_date) # 1.8.7 doesn't support to_date
+      assert_equal time.to_date, filtered
+      assert_equal nil, errors
+    else
+      assert_equal :date, errors
+    end
   end
 
   it "checks if the given date is after a certain date" do
